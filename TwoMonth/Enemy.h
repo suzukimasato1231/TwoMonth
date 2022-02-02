0cf8b2edfdcf6430ec12990f75b54cf817bedb61
LC1A_01_NakamuraRiki <k020g1212@g.neec.ac.jp>
2022-02-02 17:59:25 +0900

    enemi-bagunaosita

--- TwoMonth/Enemy.h
+++ TwoMonth/Enemy.h
@@ -83,19 +83,21 @@ private:
 	int enemyAttackDelay[enemy_Num] = { 5, 5, 5 ,5 ,5 ,5, 5, 5 ,5 ,5 ,5 };///UŒ‚‚·‚é‚Ü‚Å‚ÌŠÔ
 	int enemyColorTop[enemy_Num] = { 1, 1, 3, 2, 1, 3, 1, 1, 3, 3 ,1 };
 	int enemyColorDown[enemy_Num] = { 1, 1, 3, 2, 1, 3, 1, 1, 3, 1 ,1 };
-	int enemyColorRight[enemy_Num] = { 1, 1, 3, 2, 2, 2, 3, 3, 2, 2, 3 };
 	int enemyColorLeft[enemy_Num] = { 1, 1, 3, 2, 2, 2, 3, 2, 1, 2, 2 };
+	int enemyColorRight[enemy_Num] = { 1, 1, 3, 2, 2, 2, 3, 3, 2, 2, 3 };
 	int enemyColor[enemy_Num] = { 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 3 };
 	//‰Šú‰»—p
 	const float enemyHPKeep[enemy_Num] = { 3000, 3000, 3000, 5000, 5000, 5000, 7000, 7000, 7000, 9000, 9000 };///‘Ì—Í
 	const bool enemyIsAliveKeep[enemy_Num] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 };///‘¶İ‚·‚é‚©
 	const bool enemyIsAttackKeep[enemy_Num] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };///UŒ‚‚µ‚Ä‚¢‚é‚©
 	const int enemyAttackDelayKeep[enemy_Num] = { 5, 5, 5 ,5 ,5 ,5, 5, 5 ,5 ,5 ,5 };///UŒ‚‚·‚é‚Ü‚Å‚ÌŠÔ
-	const int enemyColorTopKeep[enemy_Num] = { 1, 1, 3, 2, 1, 3, 1, 1, 3, 3 ,1 };
-	const int enemyColorDownKeep[enemy_Num] = { 1, 1, 3, 2, 1, 3, 1, 1, 3, 1 ,1 };
+	const int enemyColorTopKeep[enemy_Num] =   { 1, 1, 3, 2, 1, 3, 1, 1, 3, 3 ,1 };
+	const int enemyColorDownKeep[enemy_Num] =  { 1, 1, 3, 2, 1, 3, 1, 1, 3, 1 ,1 };
+	const int enemyColorLeftKeep[enemy_Num] =  { 1, 1, 3, 2, 2, 2, 3, 2, 1, 2, 2 };
 	const int enemyColorRightKeep[enemy_Num] = { 1, 1, 3, 2, 2, 2, 3, 3, 2, 2, 3 };
-	const int enemyColorLeftKeep[enemy_Num] = { 1, 1, 3, 2, 2, 2, 3, 2, 1, 2, 2 };
 	const int enemyColorKeep[enemy_Num] = { 1, 2, 3, 1, 2, 3, 1, 2, 3, 2, 3 };
+
+	
 
 	int enemyAttackCount = 0;///UŒ‚‚ÌƒJƒEƒ“ƒg—p
 
